161b6f6d3a1201fa7943427ecf43518790b7f348
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-03 20:00:05 +0600

    test3

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
 app.listen(port, () => {
     console.log(`genius car server is running on ${port} port`)
 })
